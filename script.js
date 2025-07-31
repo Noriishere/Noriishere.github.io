2f616c79ada3c0b7c3fb812cda04fc6f377d6bbe
Noriishere <noriyamashin@gmail.com>
2025-07-31 19:49:01 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
-const message = `I don’t even know if you’ll ever read this.\nBut if you do… I just want you to know, I still think about you sometimes.\n\nWe both tried. We both cared. But the universe just never gave us enough space to become “us.”\n\nI just wonder… if we had met at a different point in life, would the ending have been different?\n\nAnyway, I hope you're doing okay, even though we’re not in each other’s lives anymore.\ni miss you, us, myself when us together..\n i hope you happy because your happpiness also my happiness\n i wish you and i can together in anothe universe.. or in another time once again\n I LOVE YOU AYUDYA SAFITRI\n ~ BAGAS NURDIANSYAH`;
+const signature = document.querySelector(".signature");
+signature.style.opacity = 0;
+
+const message = `I don’t even know if you’ll ever read this.\nBut if you do… I just want you to know, I still think about you sometimes.\n\nWe both tried. We both cared. But the universe just never gave us enough space to become “us.”\n\nI just wonder… if we had met at a different point in life, would the ending have been different?\n\nAnyway, I hope you're doing okay, even though we’re not in each other’s lives anymore.\ni miss you, us, myself when us together..\n\ni hope you happy because your happpiness also my happiness.\ni wish you and i can together in another universe.. or in another time once again.\n\nI LOVE YOU AYUDYA SAFITRI\n\n~ BAGAS NURDIANSYAH`;
 
 function showLetter() {
   const audio = document.getElementById("backgroundMusic");
+  const introText = document.getElementById("introText");
+  const readButton = document.querySelector(".btn");
+
   audio.play();
 
-  document.getElementById("introText").style.opacity = 0;
-  document.querySelector(".btn").style.display = "none";
+  introText.style.opacity = 0;
+  readButton.style.opacity = 0;
+
+  setTimeout(() => {
+    introText.style.display = "none";
+    readButton.style.display = "none";
+  }, 1000);
 
   setTimeout(() => {
     const letterBox = document.getElementById("letterBox");
@@ -21,10 +32,14 @@ function showLetter() {
           typedText.innerHTML += message.charAt(i);
         }
         i++;
-        setTimeout(typeWriter, 30);
+        setTimeout(typeWriter, 60);
+      } else {
+        setTimeout(() => {
+          signature.style.opacity = 1;
+        }, 1000);
       }
     }
 
     typeWriter();
-  }, 600);
+  }, 1200);
 }
